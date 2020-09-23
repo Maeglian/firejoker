9a08e899aada930560659d46ad1b52f812a1bcef
mariaisaeva <misaeva@wakeapp.ru>
2020-09-23 17:48:04 +0300

    changed animation of spinner

--- js/main.js
+++ js/main.js
@@ -6,13 +6,12 @@ $(function () {
   const link = $('.Modal-Link');
   const confetti = $('.Modal-Confetti');
   const results = ['1250$ Bonus', '25 Spins', '100$ Bonus', '155 Spins'];
-  let timer, result;
-
-  circle.addClass('anime').css('animationPlayState', 'paused');
+  const perfecthalf = ((1 / 37) * 360) / 2;
+  let currentLength = perfecthalf;
+  let result;
 
   $(document).on('click', function(event) {
     if ($(event.target).closest($('.Modal-Content')).length) return;
-    circle.css('transform', 'initial');
     $(document.body).css('overflow', '');
     modal.fadeOut();
     event.stopPropagation();
@@ -21,29 +20,31 @@ $(function () {
   link.on('mouseover', () => { confetti.show(); });
   link.on('mouseout', () => { confetti.hide(); });
 
-  $('.Spin-Spinner').on('click', function () {
-    const rTime = Math.floor(Math.random() * 5001) + 1000;
-    if (circle.css('animationPlayState') === 'paused') {
-      spinner.addClass('Spin-Spinner--active');
-      circle.css('animationPlayState', 'running');
-      timer = setTimeout(function () {
-        circle.css('animationPlayState', 'paused');
-        const angle = getRotationDegree(circle);
-        console.log(angle);
-        if (angle >= 45 && angle <= 135) result = results[1];
-        else if (angle > 135 && angle < 225) result = results[2];
-        else if (angle >= 225 && angle < 314) result = results[3];
-        else result = results[0];
-
-        prize.text(result);
-        spinner.removeClass('Spin-Spinner--active');
-        setTimeout(() => {
-          $(document.body).css('overflow', 'hidden');
-          modal.fadeIn();
-        }, 1000);
-      }, rTime);
-    }
+  spinner.on('click', function () {
+    const spininterval = getRandomInt(0, 37) * (360 / 37) + getRandomInt(3, 4) * 360;
+    currentLength += spininterval;
+    spinner.addClass('Spin-Spinner--active');
+    circle.css('transform', `rotate(${currentLength}deg)`);
   });
+
+  circle.on('transitionend', () => {
+    const angle = getRotationDegree(circle);
+    if (angle >= 45 && angle <= 135) result = results[1];
+    else if (angle > 135 && angle < 225) result = results[2];
+    else if (angle >= 225 && angle < 314) result = results[3];
+    else result = results[0];
+    prize.text(result);
+    spinner.removeClass('Spin-Spinner--active');
+
+    setTimeout(() => {
+      $(document.body).css('overflow', 'hidden');
+      modal.fadeIn();
+    }, 1000);
+  });
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   function getRotationDegree(elem) {
     const matrix = elem.css('transform');
