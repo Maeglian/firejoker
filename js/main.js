3c989b7c0a3a4f4f16d37d3faad11254edc88707
mariaisaeva <misaeva@wakeapp.ru>
2020-09-23 18:24:39 +0300

    changed way of getting angle

--- js/main.js
+++ js/main.js
@@ -28,7 +28,8 @@ $(function () {
   });
 
   circle.on('transitionend', () => {
-    const angle = getRotationDegree(circle);
+    const angle = currentLength % 360;
+    console.log(angle);
     if (angle >= 45 && angle <= 135) result = results[1];
     else if (angle > 135 && angle < 225) result = results[2];
     else if (angle >= 225 && angle < 314) result = results[3];
@@ -44,21 +45,5 @@ $(function () {
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function getRotationDegree(elem) {
-    const matrix = elem.css('transform');
-    let angle;
-
-    if (matrix !== 'none') {
-      const values = matrix.split('(')[1].split(')')[0].split(',');
-      const a = values[0];
-      const b = values[1];
-      angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-    } else angle = 0;
-
-    if (angle < 0) angle += 360;
-
-    return angle;
   }
 });
